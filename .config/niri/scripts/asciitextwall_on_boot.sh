88b2e5b3452d99d7839683ea824f2adf2efa5743
ScorpioGK <scorpiogameking@gmail.com>
2026-06-19 11:14:50 -0400

    background and neovim

--- .config/niri/scripts/asciitextwall_on_boot.sh
+++ .config/niri/scripts/asciitextwall_on_boot.sh
@@ -5,5 +5,5 @@
 # - Scorpio
 # ---------------------------------------
 export PATH="$PATH:~/Programs/AsciiTextWall"
-AsciiTextWall -t "Just Another $(date +%A)" -f "cosmic" -s 30 -w 100 -c "#ebdbb2" -b "#282828" -n 'day_of_week.png' -o '~/Pictures/Backgrounds/Static/Asciiwall/' -mf '/usr/share/fonts/TTF/AgaveNerdFontMono-Regular.ttf'
+AsciiTextWall -t "Just Another $(date +%A)" -f "random" -s 30 -w 100 -c "#ebdbb2" -b "#282828" -n 'day_of_week.png' -o '~/Pictures/Backgrounds/Static/Asciiwall/' -mf '/usr/share/fonts/TTF/AgaveNerdFontMono-Regular.ttf'
 waypaper --wallpaper '~/Pictures/Backgrounds/Static/Asciiwall/day_of_week.png'
--- .config/nvim/lua/scorpio/init.lua
+++ .config/nvim/lua/scorpio/init.lua
@@ -5,3 +5,19 @@ vim.opt.expandtab = true      -- Use spaces instead of tabs
 vim.opt.tabstop = 4           -- Display width of a tab character
 vim.opt.shiftwidth = 4        -- Number of spaces for one indent level
 vim.opt.softtabstop = 4       -- Backspace deletes this many spaces at a time   
+
+vim.opt.wrap = false
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+vim.opt.updatetime = 50
+vim.opt.colorcolumn = "120"
