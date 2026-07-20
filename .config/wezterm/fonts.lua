0c0e5fdaef8b1f3fca421094f2372ee9d5365712
ScorpioGameKing <scorpiogameking@gmail.com>
2026-07-20 08:49:44 -0400

    in the middle, font size 13

--- .config/wezterm/fonts.lua
+++ .config/wezterm/fonts.lua
@@ -4,7 +4,7 @@ local module = {}
 
 function module.font_settings(config)
     -- Set up our font features, will move to another file later
-    config.font_size = 12
+    config.font_size = 13
     config.font = wezterm.font 'Agave Nerd Font Mono'
 end
 
